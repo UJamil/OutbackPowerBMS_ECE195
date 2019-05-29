cd6f93abb4899ee53b7b906bd39c4a4be31bf5ed
Usman Jamil <usmanajamil@gmail.com>
2019-05-28 20:25:58 -0700

    updated main: now with less for loops!

--- OutbackPowerBMS_ECE195/src/main.cpp
+++ OutbackPowerBMS_ECE195/src/main.cpp
@@ -16,16 +16,20 @@
 #define NUM_CHANNELS 9
 #define RX_BUFFER_SIZE 4
 #define MESSAGE_INIT 0x80
+#define VOLTAGE_MEAS 0
+#define CURRENT_MEAS 1
+#define TEMP_MEAS 2
 
 // Function prototypes
-void setupADC(SPI *); // Setup and initialize SPI communication for ADC
-void print_ADC_value(char *, int, int);   // Console print hex values from ADC, for debugging
-float extract_float(char *); // Convert raw binary char values to floats
+void setupADC(SPI *);                   // Setup and initialize SPI communication for ADC
+void print_ADC_value(char *, int, int); // Console print hex values from ADC, for debugging
+float extractFloat(char *);             // Convert raw binary char values to floats
+float powerInst(float, float);
 
 // Global object declarations
-SPI maximADC(PA_7, PA_6, PA_5, PA_4); // SPI1_MOSI, SPI1_MISO, SPI1_SCLK, SPI1_SSEL
-SDBlockDevice sd(PA_7, PA_6, PA_5, PA_4); // SPI2_MOSI, SPI2_MISO, SPI2_SCLK, SPI2_SSEL
-CAN can1(PD_0, PD_1); // CAN1_RD, CAN1_TD
+SPI maximADC(PA_7, PA_6, PA_5, PA_4);       // SPI1_MOSI, SPI1_MISO, SPI1_SCLK, SPI1_SSEL
+SDBlockDevice sd(PB_7, PC_2, PB_13, PB_12); // SPI2_MOSI, SPI2_MISO, SPI2_SCLK, SPI2_SSEL
+CAN can1(PD_0, PD_1);                       // CAN1_RD, CAN1_TD
 
 int main()
 {
@@ -36,51 +40,44 @@ int main()
   float channel_value[NUM_CHANNELS];
   char can_message[8];
 
+  //float *powerCalcs = NULL; // Create a dynamic array for power calculations
+
   while (1)
   {
     maximADC.lock();
-    for (int i = 0; i < NUM_CHANNELS; i++) // Iterate through each ADC Channel
+    for (int i = 0; i < NUM_CHANNELS; i++) // Iterate through each ADC Channel (0-8)
     {
       maximADC.write(&conv_req, 2, adc_response, RX_BUFFER_SIZE);
-      float adc_value_f = (extract_float(adc_response)) / 1000;
+      float adc_value_f = (extractFloat(adc_response)) / 1000;
       channel_value[i] = adc_value_f;
 
+      // weird debugging thing idk
       printf("%f", adc_value_f);
       printf("\n");
 
-      conv_req += 0x08; // increment to next channel
-    }
-
-    for (int j = 0; j < NUM_CHANNELS; j += 3) // Voltage Format
-    {
-      channel_value[j] = 25.386f * channel_value[j] - 5.2756f;
-    }
-    for (int k = 1; k < NUM_CHANNELS; k += 3) // Current Format
-    {
-      channel_value[k] = (channel_value[k] - 2.5039f) * 200;
-    }
-    for (int l = 2; l < NUM_CHANNELS; l += 3) // Temperature Format
-    {
-      channel_value[l] = channel_value[l]; // TODO: Change
-    }
-
-    int counter = 0;
-    for (int i = 1; i <= 3; i++)
-    {
-      for (int j = 1; j <= 3; j++)
+      // move to function for threading
+      if (i % 3 == 0) // Voltage measurements on CH 0, 3, 6
       {
-        snprintf(can_message, 8, "%d%d%3.1f", i, j, channel_value[counter]);
-        if (can1.write(CANMessage(12, can_message, 8)))
-        {
-          printf("%s\n", can_message);
-        }
-        counter++;
-        if (counter == 9)
-        {
-          counter = 0;
-        }
-        wait_ms(20);
+        channel_value[i] = 25.386f * channel_value[i] - 5.2756f; // Voltage formatting
       }
+      else if (i % 3 == 1) // Current measurements on CH 1, 4, 7
+      {
+        channel_value[i] = (channel_value[i] - 2.5039f) * 200; // Current formatting
+      }
+      else if (i % 3 == 2) // Temperature measurements on CH 2, 5, 8
+      {
+        channel_value[i] = channel_value[i]; // Temperature formatting (change to lookup/dictionary)
+      }
+
+      snprintf(can_message, 8, "%d%d%3.1f", i, i % 3, channel_value[i]); // CH, TYP, VAL format for CAN string
+
+      // move to function for threading
+      if (can1.write(CANMessage(12, can_message, 8)))
+      {
+        printf("%s\n", can_message);
+      }
+
+      conv_req += 0x08; // increment to next channel
     }
 
     maximADC.unlock();
@@ -94,11 +91,12 @@ void setupADC(SPI *maximADC)
   maximADC->lock();
   maximADC->format(8, 0);       // 8-bit frame, pol = phase
   maximADC->frequency(1000000); // 1MHz clock frequency, looks unstable if lower?
+  
   // maximADC.write(0x10);        // Reset
   // maximADC.write(0x00);        // Buffer
 
-  maximADC->write(0x76);  // select setup register, external timing (CNVST), internal reference off (external single ended 5V), unipolar setup
-  maximADC->write(0x00);  // set all ADC channels to unipolar single-ended
+  maximADC->write(0x76); // select setup register, external timing (CNVST), internal reference off (external single ended 5V), unipolar setup
+  maximADC->write(0x00); // set all ADC channels to unipolar single-ended
   maximADC->unlock();
 }
 
@@ -113,10 +111,23 @@ void print_ADC_value(char *adc_response, int size, int ch) // for debugging, pri
 }
 
 // Helper Function
-float extract_float(char *input)
+float extractFloat(char *input)
 {
   uint16_t adc_value = ((char)input[2] + ((char)input[1] << 8));
   float adc_value_f = 1.2 * adc_value;
   return adc_value_f;
 }
 
+float powerInst(float voltage, float current)
+{
+  float instant_power = voltage * current;
+  return instant_power;
+}
+
+// void sendCAN(float)
+// {
+//   if (can1.write(CANMessage(12, can_message, 8)))
+//   {
+//     printf("%s\n", can_message);
+//   }
+// }
